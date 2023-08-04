e2b7173576769f92e05deb3f30f3efd02b4ef7e6
gayathrimaringanti <141129327+gayathrimaringanti@users.noreply.github.com>
2023-08-04 15:11:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ function makeSound(key) {
 
   switch (key) {
     case "M":
-      var audio = new Audio('sounds/tom-1.mp3');
+      var audio = new Audio('sounds/tom-2.mp3');
       audio.play();
       break;
 
     case "U":
-      var audio = new Audio('sounds/tom-2.mp3');
+      var audio = new Audio('sounds/tom-1.mp3');
       audio.play();
       break;
 
@@ -51,17 +51,17 @@ function makeSound(key) {
       break;
 
     case "C":
-      var audio = new Audio('sounds/snare.mp3');
+      var audio = new Audio('sounds/kick-bass.mp3');
       audio.play();
       break;
 
     case "O":
-      var audio = new Audio('sounds/crash.mp3');
+      var audio = new Audio('sounds/snare.mp3');
       audio.play();
       break;
 
     case "N":
-      var audio = new Audio('sounds/kick-bass.mp3');
+      var audio = new Audio('sounds/crash.mp3');
       audio.play();
       break;
    default:
